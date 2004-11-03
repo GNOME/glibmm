4cf67a4ec2c2458415b797c5e249a8cab03d5125
Cedric Gustin <cedric.gustin@swing.be>
2004-11-03 14:29:50 +0000

    Updated projects file for consistent dllexporting/dllimporting of global
    2004-11-03  Cedric Gustin  <cedric.gustin@swing.be>
    
    	* MSVC_Net2003/: Updated projects file for consistent
    	dllexporting/dllimporting of global variables.
    	* glib/glibmmconfig.h.in: Rewrote dllexport/dllimport macros for
    	MSVC for better consistency with gtkmm.
    	* build_shared/Makefile_build.am_fragment: Removed
    	$(sublib_name)_COMPILATION variable (is useless now on
    	cygwin/mingw as all symbols are dll exported).

--- MSVC_Net2003/glibmm/glibmmconfig.h
+++ MSVC_Net2003/glibmm/glibmmconfig.h
@@ -5,7 +5,7 @@
 /* version numbers */
 #define GLIBMM_MAJOR_VERSION 2
 #define GLIBMM_MINOR_VERSION 4
-#define GLIBMM_MICRO_VERSION 4
+#define GLIBMM_MICRO_VERSION 5
 
 // detect common platforms
 #if defined(_WIN32)
@@ -15,19 +15,18 @@
 #define GLIBMM_WIN32
 #define GLIBMM_DLL
 #elif defined(__CYGWIN__)
-#define GLIBMM_GCC
+#define GLIBMM_CONFIGURE
 #elif defined(__MINGW32__)
 #define GLIBMM_WIN32
-#define GLIBMM_GCC
-#define GLIBMM_DLL
+#define GLIBMM_CONFIGURE
 #else
 #warning "Unknown architecture (send me gcc --dumpspecs or equiv)"
 #endif
 #else
-#define GLIBMM_GCC
+#define GLIBMM_CONFIGURE
 #endif /* _WIN32 */
 
-#ifdef GLIBMM_GCC
+#ifdef GLIBMM_CONFIGURE
 /* compiler feature tests that are used during compile time and run-time
    by gtk-- only. tests used by gdk-- and gtk-- should go into
    gdk--config.h.in */
@@ -35,23 +34,24 @@
 /* #undef GLIBMM_CXX_HAVE_NAMESPACES */
 //#undef GLIBMM_CXX_GAUB
 //#undef GLIBMM_CXX_AMBIGUOUS_TEMPLATES
-#endif
-
-#ifdef GLIBMM_MSC
-#define GLIBMM_CXX_HAVE_MUTABLE
-#define GLIBMM_CXX_HAVE_NAMESPACES
-#pragma warning (disable: 4786 4355 4800 4181)
-#endif
-
-/* #undef GLIBMM_COMPILER_SUN_FORTE */
-
-/* #undef GLIBMM_DEBUG_REFCOUNTING */
-
 #define GLIBMM_HAVE_NAMESPACE_STD 1
 #define GLIBMM_HAVE_STD_ITERATOR_TRAITS 1
 /* #undef GLIBMM_HAVE_SUN_REVERSE_ITERATOR */
 #define GLIBMM_HAVE_TEMPLATE_SEQUENCE_CTORS 1, Defined if the STL containers have templated sequence ctors
 #define GLIBMM_HAVE_DISAMBIGUOUS_CONST_TEMPLATE_SPECIALIZATIONS 1
+/* #undef GLIBMM_COMPILER_SUN_FORTE */
+/* #undef GLIBMM_DEBUG_REFCOUNTING */
+#endif
+
+#ifdef GLIBMM_MSC
+  #define GLIBMM_CXX_HAVE_MUTABLE
+  #define GLIBMM_CXX_HAVE_NAMESPACES
+  #define GLIBMM_HAVE_NAMESPACE_STD 1
+  #define GLIBMM_HAVE_STD_ITERATOR_TRAITS 1
+  #define GLIBMM_HAVE_TEMPLATE_SEQUENCE_CTORS 1, Defined if the STL containers have templated sequence ctors
+  #define GLIBMM_HAVE_DISAMBIGUOUS_CONST_TEMPLATE_SPECIALIZATIONS 1
+  #pragma warning (disable: 4786 4355 4800 4181)
+#endif
 
 #ifndef GLIBMM_HAVE_NAMESPACE_STD
 #  define GLIBMM_USING_STD(Symbol) namespace std { using ::Symbol; }
@@ -60,15 +60,17 @@
 #endif
 
 #ifdef GLIBMM_DLL
-#if defined(GLIBMM_BUILD)
-#define GLIBMM_API __declspec(dllexport)
-#elif !defined(GLIBMM_BUILD)
-#define GLIBMM_API __declspec(dllimport)
+  #if defined(GLIBMM_BUILD) && defined(_WINDLL)
+    /* Do not dllexport as it is handled by gendef on MSVC */
+    #define GLIBMM_API
+  #elif !defined(GLIBMM_BUILD)
+    #define GLIBMM_API __declspec(dllimport)
+  #else
+    /* Build a static library */
+    #define GLIBMM_API
+  #endif /* GLIBMM_BUILD - _WINDLL */
 #else
-#define GLIBMM_API
-#endif /* glibmm_COMPILATION - DLL_EXPORT */
-#else
-#define GLIBMM_API
+  #define GLIBMM_API
 #endif /* GLIBMM_DLL */
 
 #endif /* _GLIBMM_CONFIG_H */
