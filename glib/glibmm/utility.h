ff162429862fbd69b760519e109c9d3cec3a4a4e
Bradley Bell <btb@debian.org>
2007-03-23 17:31:51 +0000

    Change variable name to avoid warnings about a shadowed member. Bug
    2007-03-19  Bradley Bell  <btb@debian.org>
    
            * glib/glibmm/helperlist.h: Change variable name to avoid
            warnings about a shadowed member.
            Bug #420316.
    
    2007-03-19  Bradley Bell  <btb@debian.org>
    
            * glib/glibmm/utility.h: remove g_free prototype, include gmem.h
            instead, to avoid a warning about a redundant declaration.
            Bug #420339.
    
    svn path=/branches/glibmm-2-12/; revision=387

--- glib/glibmm/utility.h
+++ glib/glibmm/utility.h
@@ -21,13 +21,11 @@
  */
 
 #include <glib/gmacros.h>
+#include <glib/gmem.h>
 #include <glibmm/ustring.h>
 
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-
-extern "C" void g_free(void*);
-
 
 /* Occasionally, a struct variable has to be initialized after its definition,
  * i.e. when using structs as class member data.  For convenience, the macro
