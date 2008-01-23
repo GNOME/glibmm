05f93d80001fb857bd1844854481ef2a5f6959ed
Murray Cumming <murrayc@murrayc.com>
2008-01-23 09:21:24 +0000

    Added a read() method overload with no cancellable parameter. Try loading
    2008-01-23  Murray Cumming  <murrayc@murrayc.com>
    
    * gio/src/file.ccg:
    * gio/src/file.hg: Added a read() method overload with no 
    cancellable parameter.
    * tests/giomm_simple/main.cc: Try loading the contents of a file.
    Seems to work, though there is an unknown GError domain when the file 
    does not exist.
    
    svn path=/trunk/; revision=536

--- tests/giomm_simple/main.cc
+++ tests/giomm_simple/main.cc
@@ -5,9 +5,33 @@ int main(int argc, char** argv)
 {
   Glib::init();
   Gio::init();
-   
-  Glib::RefPtr<Gio::File> file = Gio::File::create_for_path("/home/murrayc/test.txt");
- 
+
+  try
+  {
+    Glib::RefPtr<Gio::File> file = Gio::File::create_for_path("/home/murrayc/test.txt");
+    if(!file)
+      std::cerr << "Gio::File::create_for_path() returned an empty RefPtr." << std::endl;
+
+    Glib::RefPtr<Gio::FileInputStream> stream = file->read();
+    if(!stream)
+      std::cerr << "Gio::File::read() returned an empty RefPtr." << std::endl;
+
+    gchar buffer[1000]; //TODO: This is unpleasant.
+    memset(buffer, 0, 1000);
+    const gsize bytes_read = stream->read(buffer, 1000);
+    
+    if(bytes_read)
+      std::cout << "File contents read: " << buffer << std::endl;
+    else
+      std::cerr << "Gio::InputStream::read() read 0 bytes." << std::endl;
+
+  }
+  catch(const Glib::Exception& ex)
+  {
+    std::cerr << "Exception caught: " << ex.what() << std::endl; 
+  }
+
+
   return 0;
 }
 
