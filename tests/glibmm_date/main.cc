255c48cd21f29decde6a16f323823af6d2b325a8
José Alburquerque <jaalburqu@svn.gnome.org>
2008-06-10 20:32:02 +0000

    Added copy constructor and assignment operator; Made GDate constructor
    2008-06-09  José Alburquerque  <jaalburqu@svn.gnome.org>
    
    	* glib/src/date.ccg:
    	* glib/src/date.hg: Added copy constructor and assignment operator;
    	Made GDate constructor visible.
    
    	* configure.in:
    	* tests/Makefile.am:
    	* tests/glibmm_date/Makefile.am:
    	* tests/glibmm_date/main.cc: Added simple test to test Glib::Date.
    
    svn path=/trunk/; revision=661

---
+++ tests/glibmm_date/main.cc
@@ -0,0 +1,56 @@
+#include <glibmm.h>
+#include <iostream>
+
+int main(int, char**)
+{
+  Glib::Date date;
+  date.set_time_current();
+  date.add_months(1);
+  date.subtract_days(1);
+  date.add_years(1);
+
+  std::cout << "The date a year and a month from yesterday will be: " <<
+    date.get_month() << "/" << (int) date.get_day() << "/" << date.get_year() <<
+      "." << std::endl;
+
+
+  Glib::Date copy_date(date);
+  Glib::Date assigned_date;
+
+  assigned_date = copy_date;
+
+  std::cout << "The copied date is: " << copy_date.get_month() << "/" <<
+    (int) copy_date.get_day() << "/" << copy_date.get_year() << "." <<
+      std::endl;
+
+  return 0;
+}
+
+#include <glibmm.h>
+#include <iostream>
+
+int main(int, char**)
+{
+  Glib::Date date;
+  date.set_time_current();
+  date.add_months(1);
+  date.subtract_days(1);
+  date.add_years(1);
+
+  std::cout << "The date a year and a month from yesterday will be: " <<
+    date.get_month() << "/" << (int) date.get_day() << "/" << date.get_year() <<
+      "." << std::endl;
+
+
+  Glib::Date copy_date(date);
+  Glib::Date assigned_date;
+
+  assigned_date = copy_date;
+
+  std::cout << "The copied date is: " << copy_date.get_month() << "/" <<
+    (int) copy_date.get_day() << "/" << copy_date.get_year() << "." <<
+      std::endl;
+
+  return 0;
+}
+
