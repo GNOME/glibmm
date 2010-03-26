00f67118a394e743de6b87338397f2e833260495
Murray Cumming <murrayc@murrayc.com>
2010-03-26 10:41:24 +0100

    Add regression test for Bug #613250.
        * tests/Makefile.am
        * tests/glibmm_ustring_format/main.cc: Added a regression test for the
        previously-commited fix, based on code in bug #613250 from Debarshi Ray.

---
+++ tests/glibmm_ustring_format/main.cc
@@ -0,0 +1,28 @@
+#include <glibmm.h>
+
+#include <iostream>
+
+int main(int, char**)
+{
+  Glib::init();
+
+  char carr[10] = "Užduotys";
+  char * const cptr = carr;
+
+  std::wostringstream wsout;
+  wsout << carr;
+  const std::wstring& wstr = wsout.str();
+  const gunichar * const data = reinterpret_cast<const gunichar *>(
+                                  wstr.data());
+
+  //for(int i = 0; wstr.size() > i; ++i)
+  //  std::cout << data[i] << std::endl;
+
+  //Check both the const char* and char* versions.
+  Glib::ustring::format(carr);
+
+  //This threw an exception before we added a ustring::FormatStream::stream(char*) overload.
+  Glib::ustring::format(cptr);
+
+  return 0;
+}
