685d61b793b977ef2b89a6877178b8f2bc958f8b
José Alburquerque <jaalburqu@svn.gnome.org>
2010-12-28 19:42:34 -0500

    Variant test: Use the dictionary variant classes in the test.
    	* glib/src/variant.{ccg,hg} (get_iter): Make all the get_iter()
    	methods in the variant container classes const.
    	(Variant< std::pair<K, V> >::get):
    	(Variant< std::map<K, V> >::lookup):
    	(std::map<K, V> Variant< std::map<K, V> >::get): Correct the getting
    	of a std::pair<> from the Variant<> and the getting of children from
    	the parent VariantContainerBase class.
    	(Variant< std::map<K, V> >::create): Create the dictionary entries as
    	Variants and then use g_variant_builder_add_value() to add them
    	instead of g_variant_add() which is easier.
    
    	* tests/glibmm_variant/main.cc: Use the updated dictionary classes in
    	the test.

--- tests/glibmm_variant/main.cc
+++ tests/glibmm_variant/main.cc
@@ -32,9 +32,73 @@ int main(int, char**)
     "variant are " << integers_variant.get_iter().get_n_children() <<
     '.' << std::endl;
 
-  int index = 4;
+  unsigned index = 4;
   std::cout << "Element number " << index + 1 << " in the copy is " <<
     integers_variant.get(index) << '.' << std::endl;
+
+  std::cout << std::endl;
+
+  typedef std::pair<Glib::ustring, Glib::ustring> TypeDictEntry;
+
+  TypeDictEntry dict_entry("A key", "A value");
+
+  std::cout << "The original dictionary entry is (" << dict_entry.first <<
+    ", " << dict_entry.second << ")." << std::endl;
+
+  Glib::Variant<TypeDictEntry> dict_entry_variant =
+    Glib::Variant<TypeDictEntry>::create(dict_entry);
+
+  TypeDictEntry copy_entry = dict_entry_variant.get();
+
+  std::cout << "The copy dictionary entry is (" << copy_entry.first <<
+    ", " << copy_entry.second << ")." << std::endl;
+
+  std::cout << std::endl;
+
+  typedef std::map<unsigned, Glib::ustring> TypeDict;
+
+  TypeDict orig_dict;
+
+  for(unsigned i = 0; i < 10; i++)
+  {
+    std::string x_repeated(i, 'x');
+    orig_dict.insert(std::pair<unsigned, Glib::ustring>(i, x_repeated));
+  }
+
+  std::cout << "The original dictionary:" << std::endl;
+
+  for(unsigned i = 0; i < orig_dict.size(); i++)
+  {
+    std::cout << "(" << i << ", " << orig_dict[i] << ")." << std::endl;
+  }
+
+  Glib::Variant<TypeDict> orig_dict_variant =
+    Glib::Variant<TypeDict>::create(orig_dict);
+
+  TypeDict dict_copy = orig_dict_variant.get();
+
+  std::cout << "The copy of the dictionary:" << std::endl;
+
+  for(unsigned i = 0; i < dict_copy.size(); i++)
+  {
+    std::cout << "(" << i << ", " << dict_copy[i] << ")." << std::endl;
+  }
+
+  index = 3;
+
+  std::pair<unsigned, Glib::ustring> a_pair = orig_dict_variant.get(index);
+
+  std::cout << "Element number " << index + 1 << " in the variant is: (" <<
+    a_pair.first << ", " << a_pair.second << ")." << std::endl;
+
+
+  Glib::ustring value;
+
+  if(orig_dict_variant.lookup(index, value))
+  {
+    std::cout << "The x's of element number " << index + 1 <<
+      " in the variant are: " << value << '.' << std::endl;
+  }
 
   return 0;
 }
