569346a4cef42d8408428d04b70b1ceb5659793e
Krzesimir Nowak <qdlacz@gmail.com>
2011-01-31 21:38:51 +0100

    Added a primitive test script requiring some modules.
    It just checks if all modules are compilable.

---
+++ tools/pm/test.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+push (@INC, '.');
+
+require Base::Entity;
+require Base::Enum;
+require Base::Function;
+require Base::Object;
+require Base::Property;
+
+require Defs::Common;
+require Defs::Enum;
+require Defs::Function;
+require Defs::Named;
+require Defs::Object;
+require Defs::Property;
+require Defs::Signal;
