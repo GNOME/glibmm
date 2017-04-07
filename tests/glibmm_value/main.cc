17ce66a0155dc7a310ff32c8d9b0904902961bc9
Murray Cumming <murrayc@murrayc.com>
2017-04-07 21:25:48 +0200

    tests: glibmm_value: Move code to main.cc
    Which was previously almost empty.

--- tests/glibmm_value/main.cc
+++ tests/glibmm_value/main.cc
@@ -1,8 +1,37 @@
 
 #include <glibmm.h>
 
-int
-main(int, char**)
+struct Foo
 {
+  int bar;
+};
+
+namespace Gtk
+{
+class Widget;
+}
+
+void
+some_method()
+{
+  // custom copyable
+  Glib::Value<Foo> value_foo;
+
+  // custom pointer
+  Glib::Value<Foo*> value_foo_pointer;
+  Glib::Value<const Foo*> value_foo_const_pointer;
+
+  // Glib::Object pointer
+  Glib::Value<Gtk::Widget*> value_widget_pointer;
+  Glib::Value<const Gtk::Widget*> value_widget_const_pointer;
+}
+
+// Glib::Object RefPtr<>
+
+// template Glib::Value< Glib::RefPtr<Gdk::Pixbuf> >;
+// template Glib::Value< Glib::RefPtr<const Gdk::Pixbuf> >;
+//
+
+int main() {
   return EXIT_SUCCESS;
 }
