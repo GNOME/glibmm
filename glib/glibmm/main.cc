af768ca0f1571532cf31d903713e42bdccacb8af
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2018-08-31 13:04:09 +0200

    Avoid compiler warnings from function pointer conversions
    gcc8 -Wextra prints a warning when a single reinterpret_cast is used for
    conversion between different types of function pointers. The previous fix
    with a union in Glib::bitwise_equivalent_cast<>() is not standard C++.
    Rename the function to Glib::function_pointer_cast<>(), and use two
    reinterpret_casts as recommended in gcc's documentation.
    
    * glib/src/optiongroup.ccg: Use a reinterpret_cast to convert from
    a function pointer to void*. That's possible now. It's "conditionally
    supported", starting with C++11.
    
    See https://github.com/libsigcplusplus/libsigcplusplus/issues/8

--- glib/glibmm/main.cc
+++ glib/glibmm/main.cc
@@ -508,7 +508,7 @@ SignalChildWatch::connect(const sigc::slot<void(GPid, int)>& slot, GPid pid, int
   if (priority != G_PRIORITY_DEFAULT)
     g_source_set_priority(source, priority);
 
-  g_source_set_callback(source, Glib::bitwise_equivalent_cast<GSourceFunc>(&glibmm_child_watch_callback),
+  g_source_set_callback(source, Glib::function_pointer_cast<GSourceFunc>(&glibmm_child_watch_callback),
     conn_node,
     &glibmm_source_destroy_notify_callback);
 
@@ -1232,13 +1232,13 @@ IOSource::IOSource(PollFD::fd_t fd, IOCondition condition) : poll_fd_(fd, condit
 
 IOSource::IOSource(const Glib::RefPtr<IOChannel>& channel, IOCondition condition)
 : Source(g_io_create_watch(channel->gobj(), (GIOCondition)condition),
-    Glib::bitwise_equivalent_cast<GSourceFunc>(&glibmm_iosource_callback))
+    Glib::function_pointer_cast<GSourceFunc>(&glibmm_iosource_callback))
 {
 }
 
 IOSource::IOSource(GIOChannel* channel, IOCondition condition)
 : Source(g_io_create_watch(channel, (GIOCondition)condition),
-    Glib::bitwise_equivalent_cast<GSourceFunc>(&glibmm_iosource_callback))
+    Glib::function_pointer_cast<GSourceFunc>(&glibmm_iosource_callback))
 {
 }
 
