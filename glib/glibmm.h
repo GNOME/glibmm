906cb65e00fc14fca64db873c26a9f7d99eaed88
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2018-10-31 13:33:58 +0100

    Change the ABI to glibmm-2.60
    So we can use the 2.58 version numbers for stable releases in the
    glibmm-2.4 ABI series.
    We don't need to release a stable ABI-parallel glibmm until
    we need to release gtkmm 4.0, and that won't happen until GTK+ 4.0.0
    happens, and we don't know when that might be.
    
    We've done similar ABI name changes several times before.

--- glib/glibmm.h
+++ glib/glibmm.h
@@ -64,12 +64,12 @@
  *
  * If your  source file is @c program.cc, you can compile it with:
  * @code
- * g++ program.cc -o program  `pkg-config --cflags --libs glibmm-2.58 giomm-2.58`
+ * g++ program.cc -o program  `pkg-config --cflags --libs glibmm-2.60 giomm-2.60`
  * @endcode
  *
  * Alternatively, if using autoconf, use the following in @c configure.ac:
  * @code
- * PKG_CHECK_MODULES([GLIBMM], [glibmm-2.58 giomm-2.58])
+ * PKG_CHECK_MODULES([GLIBMM], [glibmm-2.60 giomm-2.60])
  * @endcode
  * Then use the generated @c GLIBMM_CFLAGS and @c GLIBMM_LIBS variables in the
  * project Makefile.am files. For example:
