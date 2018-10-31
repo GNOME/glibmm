f4fea71f59f6a5a02992fd219972a616a69b5d41
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2018-10-31 12:40:41 +0100

    Add tools/gen_scripts/generate_all.sh

---
+++ tools/gen_scripts/generate_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Regenerate all glibmm's docs.xml and .defs files
+
+./glib_generate_docs.sh
+./glib_generate_enums.sh
+./glib_generate_extra_defs.sh
+./glib_generate_methods.sh
+
+./gio_generate_docs.sh
+./gio_generate_enums.sh
+./gio_generate_extra_defs.sh
+./gio_generate_methods.sh
