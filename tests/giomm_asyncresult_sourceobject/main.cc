0912d61065490d265ad868d8af66fc65648b300f
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2019-03-18 15:54:51 +0100

    Gio::AsyncResult: Explain why wrap() is not used in get_source_object_base()
    * gio/src/asyncresult.ccg: Replace a TODO comment with an explanation.
    * tests/giomm_asyncresult_sourceobject/main.cc: Add code that demonstrates
    why Glib::wrap(GObject* object, bool take_copy) returns an empty RefPtr.

--- tests/giomm_asyncresult_sourceobject/main.cc
+++ tests/giomm_asyncresult_sourceobject/main.cc
@@ -1,5 +1,6 @@
 #include <giomm.h>
 #include <iostream>
+#include <typeinfo>
 
 void
 on_read_async(const Glib::RefPtr<Gio::AsyncResult>& result)
@@ -10,10 +11,24 @@ on_read_async(const Glib::RefPtr<Gio::AsyncResult>& result)
     exit(EXIT_FAILURE);
   }
 
-  if (!g_async_result_get_source_object(result->gobj()))
+  auto cobj = g_async_result_get_source_object(result->gobj());
+  if (!cobj)
   {
     std::cerr << G_STRFUNC << ": g_async_result_get_source_object() failed." << std::endl;
     exit(EXIT_FAILURE);
+  }
+
+  // Show why Glib::wrap(cobj) can't be used in Gio::AsyncResult::get_source_object_base().
+  // cppobjbase is not a Glib::Object*, it's a Gio::File* which is a Glib::Interface*.
+  std::cout << "GType name: " << G_OBJECT_TYPE_NAME(cobj) << std::endl;
+  auto cppobjbase = Glib::wrap_auto(cobj); // Glib::ObjectBase::_get_current_wrapper(cobj);
+  if (cppobjbase)
+  {
+    std::cout << "C++ type name: " << typeid(*cppobjbase).name() << std::endl;
+    auto cppobj = dynamic_cast<Glib::Object*>(cppobjbase); // Part of Glib::wrap(GObject*, bool)
+    auto cppiface = dynamic_cast<Glib::Interface*>(cppobjbase);
+    std::cout << "dynamic_cast<Glib::Object*>: " << cppobj << std::endl;
+    std::cout << "dynamic_cast<Glib::Interface*>: " << cppiface << std::endl;
   }
 
   if (!result->get_source_object_base())
