GNOME/glibmm

commit 8b8af81f7325a03e1aef853524272be9675b62ab

ustring: Silence warning if MSC_VER is undefined/0

glib/glibmm/ustring.h
@@ -30,7 +30,7 @@
 #endif
 
 /* work around linker error on Visual Studio if we don't have GLIBMM_HAVE_ALLOWS_STATIC_INLINE_NPOS */
-#if (_MSC_VER >= 1600) && !defined (GLIBMM_HAVE_ALLOWS_STATIC_INLINE_NPOS)
+#if defined(MSC_VER) && MSC_VER >= 1600 && !defined(GLIBMM_HAVE_ALLOWS_STATIC_INLINE_NPOS)
 const std::basic_string<char>::size_type std::basic_string<char>::npos = (std::basic_string<char>::size_type) -1;
 #endif
 

commit ed96df5fa32ed543a51401cfc030939210055e14

glib/glibmm/ustring.h: Fix building on Visual Studio 2015
Commit 8b8af81f accidentally changed the macro check from '_MSC_VER' to
'MSC_VER', fix that, so that things build correctly on Visual Studio 2015

glib/glibmm/ustring.h
@@ -30,7 +30,7 @@
 #endif
 
 /* work around linker error on Visual Studio if we don't have GLIBMM_HAVE_ALLOWS_STATIC_INLINE_NPOS */
-#if defined(MSC_VER) && MSC_VER >= 1600 && !defined(GLIBMM_HAVE_ALLOWS_STATIC_INLINE_NPOS)
+#if defined(_MSC_VER) && _MSC_VER >= 1600 && !defined(GLIBMM_HAVE_ALLOWS_STATIC_INLINE_NPOS)
 const std::basic_string<char>::size_type std::basic_string<char>::npos = (std::basic_string<char>::size_type) -1;
 #endif
 