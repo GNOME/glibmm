98e41f19cd6f4089f815b8535c9ec12051f805a1
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2021-03-26 15:44:04 +0100

    Meson build: Make it possible to use glibmm as a subproject
    glib and sigc++ can be subprojects of glibmm.

--- gio/giomm/slot_async.cc
+++ gio/giomm/slot_async.cc
@@ -14,7 +14,7 @@
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "slot_async.h"
+#include <giomm/slot_async.h>
 #include <glibmm/exceptionhandler.h>
 #include <giomm/asyncresult.h>
 
