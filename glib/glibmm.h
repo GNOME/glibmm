5a4abcf76a73864e76af43f50332ee68ca6c38df
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2021-05-19 16:01:10 +0200

    Documentation: Let links point to glibmm-2.4 and gtkmm-3.0 versions

--- glib/glibmm.h
+++ glib/glibmm.h
@@ -26,7 +26,7 @@
  * glibmm is the official C++ interface for the popular cross-platform library %Glib.
  * It provides non-UI API that is not available in standard C++ and makes it
  * possible for gtkmm to wrap GObject-based APIs.
- * See also the <a href="https://library.gnome.org/devel/gtkmm-tutorial/stable/">
+ * See also the <a href="https://library.gnome.org/devel/gtkmm-tutorial/3.24/">
  * Programming with gtkmm</a> book for a tutorial on programming with gtkmm and
  * glibmm.
  *
